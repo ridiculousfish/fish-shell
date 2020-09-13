591888872c36f4e5dadd078c2d9b916e4bf60a01
ridiculousfish <rf@fishshell.com>
2020-09-13 16:23:55 -0700

    disown-parent test to require non-concurrent mode

--- tests/checks/disown-parent.fish
+++ tests/checks/disown-parent.fish
@@ -1,8 +1,10 @@
-# RUN: env fish_test_helper=%fish_test_helper %fish %s
+# RUN: env fish_test_helper=%fish_test_helper %fish --features=no-concurrent %s
 
 # Ensure that a job which attempts to disown itself does not explode.
 # Here fish_test_helper is the process group leader; we attempt to disown
 # its pid within a pipeline containing it.
+# This requires non-concurrent mode; in concurrent mode it doesn't get
+# its own pgroup.
 
 function disowner
     read -l pid
