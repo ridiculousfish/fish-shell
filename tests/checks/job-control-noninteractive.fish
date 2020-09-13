c6cec5b69fb7ecb9b9b7efde5f7e080ae01092a9
ridiculousfish <rf@fishshell.com>
2020-09-13 11:45:47 -0700

    job-control-noninteractive test to not put read in pipelines
    Under concurrent execution, any pipeline that contains a builtin or fish
    function must necessarily live in fish's prgroup. Switch the test from
    piping an external process to read, to instead redirecting to a file and
    reading from the file.

--- tests/checks/job-control-noninteractive.fish
+++ tests/checks/job-control-noninteractive.fish
@@ -6,12 +6,15 @@ status job-control full
 /bin/echo hello
 #CHECK: hello
 
-$fth print_pgrp | read first
-$fth print_pgrp | read second
+set tmppath (mktemp)
+$fth print_pgrp > $tmppath
+$fth print_pgrp >> $tmppath
+read --line first second < $tmppath
 test $first -ne $second
 and echo "pgroups differed, meaning job control worked"
 or echo "pgroups were the same, job control did not work"
 #CHECK: pgroups differed, meaning job control worked
+rm $tmppath
 
 # fish ignores SIGTTIN and so may transfer the tty even if it
 # doesn't own the tty. Ensure that doesn't happen.
