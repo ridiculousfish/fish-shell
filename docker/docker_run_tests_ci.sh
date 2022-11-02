1acb7a9295751d1ef3eb522c3b43229b8428de0e
ridiculousfish <rf@fishshell.com>
2022-11-01 20:49:16 -0700

    thrashing

--- docker/docker_run_tests_ci.sh
+++ docker/docker_run_tests_ci.sh
@@ -11,6 +11,7 @@ DOCKER_EXTRA_ARGS=""
 
 # Exit on failure.
 set -e
+set -x
 
 # Get fish source directory.
 FISH_SRC_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")"/.. >/dev/null && pwd)
@@ -29,15 +30,17 @@ fi
 # If we are running docker-in-docker, as we are in CI, then our fish source
 # directory will not mount properly in the inner image. So use run with sleep
 # instead, then later exec.
+# We need seccomp=unconfined otherwise posix_spawn barfs on fedora.
 CONTAINER_ID=$(
     docker run \
         --detach \
         --rm \
+        --security-opt seccomp=unconfined \
         $DOCKER_EXTRA_ARGS \
-        "$DOCKER_IMAGE"
+        "$DOCKER_IMAGE" \
         sleep infinity
 )
 
 docker cp "$FISH_SRC_DIR/." "$CONTAINER_ID":/fish-source/
 docker exec --user root "$CONTAINER_ID" chown -R fishuser /fish-source
-docker exec --user fishuser "$CONTAINER_ID" /fish_run_tests.sh
+docker exec --user fishuser "$CONTAINER_ID" /fish-source/docker/context/fish_run_tests.sh
