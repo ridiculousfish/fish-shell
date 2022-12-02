ad0cdc8dc7c4364a997c33b3d1e8c9c0b16286d2
ridiculousfish <rf@fishshell.com>
2022-12-02 14:57:29 -0800

    Reducing

--- tests/checks/noshebang.fish
+++ tests/checks/noshebang.fish
@@ -5,36 +5,6 @@
 set testdir (mktemp -d)
 cd $testdir
 
-touch file
-chmod a+x file
-
-function runfile
-    # Run our file twice, printing status.
-    # Arguments are passed to exercise the re-execve code paths; they have no other effect.
-    true # clear status
-    set -g fish_use_posix_spawn 1
-    ./file arg1 arg2 arg3
-    echo $status
-
-    true # clear status
-    set -g fish_use_posix_spawn 0
-    ./file arg1 arg2 arg3 arg4 arg5
-    echo $status
-end
-
-# Empty executable files are 'true'.
-true >file
-sleep 0.1
-runfile
-#CHECK: 0
-#CHECK: 0
-
-# Files without NUL are 'true' as well.
-echo -e -n '#COMMENT\n#COMMENT' >file
-runfile
-#CHECK: 0
-#CHECK: 0
-
 # Never implicitly pass files ending with .fish to /bin/sh.
 true >file.fish
 sleep 0.1
